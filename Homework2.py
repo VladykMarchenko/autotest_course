d7130875c76c4a77852268551461277b690b5961
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-04-03 13:54:22 +0300

    fix homework2

--- Homework2.py
+++ Homework2.py
@@ -1,2 +1,2 @@
-animals_names = input("Murzik, Barsik, Pantera")
-print(animals_names)
+animals_names1 = ['Murzik', 'Barsik', 'Pantera']
+print(animals_names1[0], animals_names1[1], animals_names1[2],  sep=', ')
