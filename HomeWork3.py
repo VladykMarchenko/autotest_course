e661b85ea75155b620ec894fe71cd880014512d5
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-04-03 13:51:13 +0300

    fix homework

--- HomeWork3.py
+++ HomeWork3.py
@@ -1,10 +1,11 @@
-countries = "Ukraine, Spain, Italy"
-a1 = "Ukraine:"
-b1 = " Kuiv"
-a2 = "Spain:"
-b2 = " Madrid"
-a3 = "Italy:"
-b3 = " Rome"
-print(a1+b1)
-print(a2+b2)
-print(a3+b3)
+countries = ['Ukraine', 'Spain', 'Italy']
+UA = {countries[0]: "Kyiv"}
+for key in UA:
+    print(key, UA[key], sep=': ')
+SP = {countries[1]: "Madrid"}
+for key in SP:
+    print(key, SP[key], sep=': ')
+IT = {countries[2]: "ROME"}
+for key in IT:
+    print(key, IT[key], sep=': ')
+
