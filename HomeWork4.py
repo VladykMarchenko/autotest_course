3341ec0a222bf2cdacf970eb1940bbbccb7f3639
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-04-03 18:02:42 +0300

    che

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,9 +1,7 @@
-a = 3
-b = 5
-print(a+b)
-print(a*b)
-if a + b == 8:
-    print("3+5=8")
-if a * b == 15:
-    print("3*5=15")
+numbers = [3,5]
+sum_numbers = (numbers[0] + numbers[1])
+multiply_numbers = numbers[0] * numbers[1]
+print(numbers[0], '+', numbers[1], '=', str(sum_numbers))
+print(numbers[0], '*', numbers[1], '=', str(multiply_numbers))
+
 
