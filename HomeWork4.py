91e9d7718d909dd8e704b7aaa371ea17a94a378e
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-04-05 12:40:05 +0300

    fix hadcode

--- HomeWork4.py
+++ HomeWork4.py
@@ -1,7 +1,14 @@
-numbers = [3,5]
-sum_numbers = (numbers[0] + numbers[1])
-multiply_numbers = numbers[0] * numbers[1]
-print(numbers[0], '+', numbers[1], '=', str(sum_numbers))
-print(numbers[0], '*', numbers[1], '=', str(multiply_numbers))
+# numbers = [3,5]
+# sum_numbers = (numbers[0] + numbers[1])
+# multiply_numbers = numbers[0] * numbers[1]
+# print(numbers[0], '+', numbers[1], '=', str(sum_numbers))
+# print(numbers[0], '*', numbers[1], '=', str(multiply_numbers))
 
-
+number1 = input("Enter a ")
+print(number1)
+number2 = input("Enter b")
+print(number2)
+sum_number = int(number1) + int(number2)
+multiply_number = int(number1) * int(number2)
+print(number1, '+', number2, '=', sum_number)
+print(number1, '+', number2, '=', multiply_number)
