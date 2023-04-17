9fd01ba13c4afda68db675b6de52dfb28f7201e0
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-04-17 15:16:57 +0300

    sum number words

--- homework11.py
+++ homework11.py
@@ -1,4 +1,12 @@
 multi_string = "Hello all. Here's pretty cold and hot. Choose yourself."
-result = len(multi_string.split())
-print(str(result))
+multi_string2 = multi_string.split('.') # разбиваем строчку на предложения
+words_number = []
+for sentence in multi_string2:
+    words = sentence.split(' ')  # циклом проходимся по каждому предложению и разбиваем на слова
+    count = 0
+    for word in words:
+        if word != '':
+            count += 1
+    words_number.append(count)
+print(words_number[:-1])
 
