5226cc3122bde97276f1cba6a392f89432178872
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-04-21 16:57:51 +0300

    fix constant

--- homework15.py
+++ homework15.py
@@ -1,12 +1,29 @@
-lst = [2, 4, 6, 2, 1, 1, 9, 4, 6]
-lst2 = []
+lst = [2, 4, 6, 2, 1, 1, 9, 4, 6] #MIN = 3, MAX = 6
+MIN = 3
+MAX = 6
+
+sum_ = 0
+product = 1
 for number in lst:
-    if number >= 3 and number <= 6:
-        lst2.append(number)
-sum_ = sum(lst2)
-product = (int(lst2[0]) * int(lst2[1]) * int(lst2[2]) * int(lst2[3]))
+    if number >= MIN and number <= MAX:
+        sum_ += number
+        product *= number
 print(sum_)
 print(product)
+
+
+#Такие вещи лучше не хардкодить, а выносить в константы или переменные
+
+# if number >= 3 and number <= 6:
+#
+#
+# Элементы списка и так уже числа, поэтому еще раз конвертировать смысла нет, тем более если там будет флоат?
+#
+# product = (int(lst2[0]) * int(lst2[1]) * int(lst2[2]) * int(lst2[3]))
+#
+#
+# Основной момент, код не работает, если в отфильтрованном списке будет не 4 элемента
+
 
 
 
