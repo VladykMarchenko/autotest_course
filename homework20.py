f37ffa3421a64296febf947b5f9efbc67b9a07f6
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-04-30 11:13:54 +0300

    fix code

--- homework20.py
+++ homework20.py
@@ -1,12 +1,8 @@
-t = []
-with open('txt.txt') as f:
-    for i, line in enumerate(f):
-            t.append(line)
-print(sorted(str(input(line).split()[-1])))
-
-
-
-
-
-
-
+file = open('txt.txt')
+max_word = ''
+for line in file:
+    words = line.split()
+    for word in words:
+        if len(word) > len(max_word):
+            max_word = word
+print(max_word)
