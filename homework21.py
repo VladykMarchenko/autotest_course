ee218b163a0ffcb743f4a021fdca268e624f1642
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-05-20 20:08:35 +0300

    fix txt parcing zolupa

--- homework21.py
+++ homework21.py
@@ -1,26 +1,30 @@
+import json
+
+file_name = 'users.txt'
+out_file_txt = 'user_out.txt'
+out_file_json = 'users.json'
+
 def log_txt(dump):
-    with open("user_out.txt", "w") as wt:
-        wt.write(dump)
+    with open(out_file_txt, "w") as wt: #wt alliace
+        wt.writelines(dump)
 
 
 def log_json(dump):
-    import json
 
-    with open("users.json", "w") as wj:
+    with open(out_file_json, "w") as wj:
         wj.write(json.dumps(dump))
 
 
-def obtain_logs(file_path):
+def obtain_logs_json(file_path):
     lines = open(file_path).readlines()
     res_json = []
-    res_txt = []
 
     for line in lines:
         line_strip = line.strip()
 
         if line_strip != "":
             line_list = line_strip.split(";")
-            name = line_list[0]
+            name = line_list[0].split()
             age = line_list[1]
             phones = [phone.strip() for phone in line_list[2].split(",")]
 
@@ -33,24 +37,41 @@ def obtain_logs(file_path):
                 "age": int(age) if age and age.isalnum() else None,
                 "phones": phones if line_list[2] else []
             }
-
-            dump_txt = {
-                "name": name,
-                "age": int(age) if age and age.isalnum() else "",
-                "phones": phones if line_list[2] else ""
-            }
-
             res_json.append(dump_json)
-            res_txt.append(dump_txt)
+
 
     return {
-        "txt": str(res_txt),
-        "json": res_json,
+        "json": res_json
     }
 
-dumps = obtain_logs('users.txt')
+dumps = obtain_logs_json(file_name)
 
-log_txt(dumps["txt"])
 log_json(dumps["json"])
 
 
+def obtain_logs_txt(file_path):
+    lines = open(file_path).readlines()
+    result_txt = []
+    for line in lines:
+        line_strip = line.strip()
+
+        if line_strip != "":
+            line_list = line_strip.split(";")
+            name = line_list[0].strip()
+            age = line_list[1] if line_list[1].isalnum() else ""
+            phones = [phone.strip() for phone in line_list[2].split(",")]
+            phone1 = phones if line_list[2] else ""
+            phones_users = ''
+        for ph_users in phone1:
+            phones_users += str(ph_users)
+
+        l = []
+        for phone in line_list[2].split(","):
+            l.append(phone.strip())
+        result_txt.append(name  + ";" + age + ";" + phones_users + "\n")
+
+    return result_txt
+
+dumps2 = obtain_logs_txt(file_name)
+
+log_txt(dumps2)
