71f0b7b0512ef33997315234c9ee2f24a96309d3
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-06-02 13:31:18 +0300

    fix generation m and n

--- homework27.py
+++ homework27.py
@@ -1,12 +1,8 @@
 def gen (m,n):
-        for i in range(1,n +1):
-            yield i
-        for i in range(1,n +1):
-            yield i**2
-        for i in range(1,n +1):
-            yield i**m
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                yield j ** i
 for i in gen(3,4):
     print(i, end=" ")
-
 
 
