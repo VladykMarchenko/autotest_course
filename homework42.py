15bf24af68c97a07c3bc4c782636caa43616e64e
vladmarchenko <vladyslavmarchenko@orderry.com>
2023-06-10 15:47:18 +0300

    5 css and 5 x-path selectors

---
+++ homework42.py
@@ -0,0 +1,22 @@
+"""
+https://web.remonline.app/login
+CSS locators
+#login поиск по ид
+[name=login] поиск по name
+form > div > div:nth-child(2) > :nth-child(2) > :nth-child(1) >:nth-child(1) >:nth-child(1) поиск по чаилд от тега
+
+button.Jk792 > [class=I7c3b] От тега по классу и ещё раз по классу
+div.mV2Wg > [style] > div от дива с классом, до стайла и снова на див
+
+x-path locators
+//*[@id="login"] поиск по ид
+//input[@class="tTiE6"] по тегу и классу
+//input[@name="login"] поиск по тегу и имени
+
+//button[@type='submit']/div поиск по тегу, классу и к следующему тегу
+//div[@class="xjH_e"]/../div ищем пок классу, поднимаемся вверх выше по уровню вложенности, опускаемся вниз по див
+"""
+
+
+
+
